74a3690a60dc7bc415abf7ec0c8e935c1073492a
Mustafa Culha <merguven753@gmail.com>
2023-01-31 14:15:48 +0300

    JDBC

--- src/test/java/JDBC02_execute_executeUpdate.java
+++ src/test/java/JDBC02_execute_executeUpdate.java
@@ -1,41 +1,92 @@
 import com.mysql.cj.x.protobuf.MysqlxPrepare;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBC02_execute_executeUpdate {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         /*
-    A) CREATE TABLE, DROP TABLE, ALTER TABLE gibi DDL ifadeleri icin sonuc kümesi (ResultSet)
-       dondurmeyen metotlar kullanilmalidir. Bunun icin JDBC'de 2 alternatif bulunmaktadir.
-        1) execute() metodu-boolean döndürür
-        2) executeUpdate() metodu-int değer döndürür.
-    B) - execute() metodu her tur SQL ifadesiyle kullanilabilen genel bir komuttur.
-       - execute(), Boolean bir deger dondurur. DDL islemlerinde false dondururken,
-         DML islemlerinde true deger dondurur.
-       - Ozellikle, hangi tip SQL ifadesine hangi methodun uygun olduğu bilenemediği
-         durumlarda tercih edilmektedir.
-    C) - executeUpdate() metodu ise INSERT, Update gibi DML islemlerinde yaygin kullanilir.
-       - bu islemlerde islemden etkilenen satir sayisini dondurur.
-       - Ayrıca, DDL islemlerinde de kullanilabilir ve bu islemlerde 0 dondurur.
-    */
+ 	A) CREATE TABLE, DROP TABLE, ALTER TABLE gibi DDL ifadeleri icin sonuc kümesi (ResultSet)
+ 	   dondurmeyen metotlar kullanilmalidir. Bunun icin JDBC'de 2 alternatif bulunmaktadir.
 
+ 	    1) execute() metodu - boolean dondurur.
+ 	    2) executeUpdate() metodu - int deger dondurur.
+
+ 	B) - execute() metodu her tur SQL ifadesiyle kullanilabilen genel bir komuttur.
+ 	   - execute(), Boolean bir deger dondurur. DDL islemlerinde false dondururken,
+ 	     DML islemlerinde true deger dondurur.
+ 	   - Ozellikle, hangi tip SQL ifadesine hangi metodun uygun oldugunun bilinemedigi
+ 	     durumlarda tercih edilmektedir.
+
+ 	C) - executeUpdate() metodu ise INSERT, Update gibi DML islemlerinde yaygin kullanilir.
+ 	   - bu islemlerde islemden etkilenen satir sayisini dondurur.
+ 	   - Ayrıca, DDL islemlerinde de kullanilabilir ve bu islemlerde 0 dondurur.
+    */
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys?serverTimezone=UTC", "root", "1234");
         Statement st = con.createStatement();
 
-
         /*======================================================================
-        ORNEK1: ogrenciler tablosunu siliniz.
-        ========================================================================*/
+		  ORNEK1: isciler tablosunu siliniz.
+ 	    ========================================================================*/
 
-        String dropQuery="drop table ogrenciler";
-        System.out.println(st.execute(dropQuery));
+        String dropQuery = "DROP TABLE isciler";
 
+        // System.out.println(st.execute(dropQuery));
 
+        if (!st.execute(dropQuery)){
+            System.out.println("Isciler tablosu silindi!");
+        }
+
+        /*=======================================================================
+          ORNEK2: isciler adinda bir tablo olusturunuz id int,
+          birim VARCHAR(10), maas int
+	    ========================================================================*/
+
+        String createTable = "CREATE TABLE isciler" +
+                "(id INT, " +
+                "birim VARCHAR(10), " +
+                "maas INT)";
+
+        if (!st.execute(createTable)){
+            System.out.println("Isciler tablosu olusturuldu!");
+        }
+
+        /*=======================================================================
+		  ORNEK3: isciler tablosuna yeni bir kayit (80, 'ARGE', 4000)
+		  ekleyelim.
+		========================================================================*/
+
+        String insertData = "INSERT INTO isciler VALUES(80, 'ARGE', 4000)";
+
+        System.out.println("Islemden etkilenen satir sayisi : " + st.executeUpdate(insertData));
+
+        /*=======================================================================
+          ORNEK4: isciler tablosuna birden fazla yeni kayıt ekleyelim.
+            INSERT INTO isciler VALUES(70, 'HR', 5000)
+            INSERT INTO isciler VALUES(60, 'LAB', 3000)
+            INSERT INTO isciler VALUES(50, 'ARGE', 4000)
+         ========================================================================*/
+
+        String[] queries={"INSERT INTO isciler VALUES(70, 'HR', 5000)"
+                           ,"INSERT INTO isciler VALUES(60, 'LAB', 3000)"
+                           ,"INSERT INTO isciler VALUES(50, 'ARGE', 4000)"};
+
+        int count=0;
+        for (String each:queries) {
+           count +=st.executeUpdate(each);
+
+        }
+        System.out.println(count+" satır eklendi...");
+
+        String selectQuery="select * from isciler";
+
+        ResultSet rs=st.executeQuery(selectQuery);
+
+        while (rs.next()){
+            System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
+        }
     }
+
 }
